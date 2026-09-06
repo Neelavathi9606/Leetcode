c45cf4c28b0fc0f0f848455691ded44abfda0961
Neelavathi9606 <neela.j9606@gmail.com>
2026-09-06 14:43:43 +0530

    Time: 2 ms (70.56%) | Memory: 55.8 MB (17.89%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        for(int i = 0; i < nums.length; i++) {
+       /* for(int i = 0; i < nums.length; i++) {
             int count = 0;
             
             for(int j = 0; j < nums.length; j++) {
@@ -16,6 +16,37 @@ class Solution {
         }
         
         return -1;
+    }*/
+    int element = 0;
+        int count = 0;
+
+        // Step 1: Find candidate
+        for (int num : nums) {
+            if (count == 0) {
+                element = num;
+            }
+
+            if (num == element) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        // Step 2: Verify candidate
+        count = 0;
+        for (int num : nums) {
+            if (num == element) {
+                count++;
+            }
+        }
+
+        if (count > nums.length / 2) {
+            return element;
+        }
+
+        return -1; // No majority element
     }
+
 }
     
