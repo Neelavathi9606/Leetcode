35434602982d40db592f97df846193b7b74b0062
Neelavathi9606 <neela.j9606@gmail.com>
2026-09-10 11:47:47 +0530

    Time: 1 ms (85.42%) | Memory: 44.2 MB (92.69%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans=new ArrayList<>();
+        StringBuilder current=new StringBuilder();
+        backtrack(n,0,0,current,ans);
+        return ans;
+        
+    }
+    private void backtrack(int n,int open,int close,StringBuilder current,List<String> ans){
+        if(current.length()==2*n){
+            ans.add(current.toString());
+            return;
+        }
+        if(open<n){
+            current.append('(');
+            backtrack(n,open+1,close,current,ans);
+            current.deleteCharAt(current.length() -1);
+
+        }
+        if(close<open){
+            current.append(')');
+            backtrack(n,open,close+1,current,ans);
+            current.deleteCharAt(current.length() -1);
+        }
+    }
+}
